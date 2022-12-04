2e17e2b3353113ca2f9b76339a939a2443577f8f
A Stone <simplererstone@gmail.com>
2022-12-04 15:00:45 -0500

    started content and styling for quiz

--- assets/script.js
+++ assets/script.js
@@ -1,25 +1,23 @@
-var header = document.createElement("h1");
-
-header.textContent = "Here is a connection";
-
-document.body.appendChild(header)
-
-var nextEl = document.querySelector('#next');
+var nextEl = document.querySelector("#next");
+var topScores = document.querySelector("#topscores")
+var questionEl = document.querySelector(".container")
 var cursor = 0;
 
-var questions = ["a", "b", "c", "d"];
 
-var displayQuestion = function() {
-    nextEl.textContent = questions[cursor];
-};
 
-var advance = function() {
-    if (cursor < questions.length - 1) {
-        cursor++;
-        displayQuestion()
-    }
-};
+// var questions = ["a", "b", "c", "d"];
 
-nextEl.addEventListener('click', advance);
+// var displayQuestion = function() {
+//     nextEl.textContent = questions[cursor];
+// };
 
-displayQuestion();
+// var advance = function() {
+//     if (cursor < questions.length - 1) {
+//         cursor++;
+//         displayQuestion()
+//     }
+// };
+
+// nextEl.addEventListener('click', advance);
+
+// displayQuestion();
