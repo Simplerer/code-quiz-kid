266f97b7cbdd2dc167a3d14ac6c32f2a729092d3
A Stone <simplererstone@gmail.com>
2022-12-04 16:36:20 -0500

    connecting changing questions to correct answers

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,21 @@
 
-var topScores = document.querySelector("#topscores");
+// var topScores = document.querySelector("#topscores");
 var containers = document.querySelectorAll(".container");
 var cursor = 0;
 
+
+var startTimer = function() {
+
+    nextQuestion();
+}
 var showQuestions = function() {
     for (var item of containers) {
         console.log(item);
         if (item.dataset.index != cursor) {
 
             item.style.display = "none";
+        } else {
+            item.style.display = "block";
         }
 }
 }
@@ -16,31 +23,19 @@ var showQuestions = function() {
 var nextQuestion = function(event) {
     var element = event.target;
     
-    if (element.matches("container"))  {
-        if (cursor < containers.length) {
+    if (element.matches(".container button"))  {
+        if (cursor < containers.length - 1) {         
+            
             cursor++;
+        
 
     }
     showQuestions();
 }
 }
 
+document.addEventListener('click', startTimer);
+document.addEventListener('click', nextQuestion);
 showQuestions();
 
 
-// var questions = ["a", "b", "c", "d"];
-
-// var displayQuestion = function() {
-//     nextEl.textContent = questions[cursor];
-// };
-
-// var advance = function() {
-//     if (cursor < questions.length - 1) {
-//         cursor++;
-//         displayQuestion()
-//     }
-// };
-
-// nextEl.addEventListener('click', advance);
-
-// displayQuestion();
