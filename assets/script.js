078c585a300ec948aaa00ddd6d781f7e7cbc1cdb
A Stone <simplererstone@gmail.com>
2022-12-04 00:10:00 -0500

    added instructors base hint for clicking through items

--- assets/script.js
+++ assets/script.js
@@ -4,3 +4,22 @@ header.textContent = "Here is a connection";
 
 document.body.appendChild(header)
 
+var nextEl = document.querySelector('#next');
+var cursor = 0;
+
+var questions = ["a", "b", "c", "d"];
+
+var displayQuestion = function() {
+    nextEl.textContent = questions[cursor];
+};
+
+var advance = function() {
+    if (cursor < questions.length - 1) {
+        cursor++;
+        displayQuestion()
+    }
+};
+
+nextEl.addEventListener('click', advance);
+
+displayQuestion();
