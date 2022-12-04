9e393acd45926056aec9c1c7a14b6a149f1ba8c9
A Stone <simplererstone@gmail.com>
2022-12-04 16:03:56 -0500

    working logic to target one question at a time

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,31 @@
-var nextEl = document.querySelector("#next");
-var topScores = document.querySelector("#topscores")
-var questionEl = document.querySelector(".container")
+
+var topScores = document.querySelector("#topscores");
+var containers = document.querySelectorAll(".container");
 var cursor = 0;
 
+var showQuestions = function() {
+    for (var item of containers) {
+        console.log(item);
+        if (item.dataset.index != cursor) {
+
+            item.style.display = "none";
+        }
+}
+}
+
+var nextQuestion = function(event) {
+    var element = event.target;
+    
+    if (element.matches("container"))  {
+        if (cursor < containers.length) {
+            cursor++;
+
+    }
+    showQuestions();
+}
+}
+
+showQuestions();
 
 
 // var questions = ["a", "b", "c", "d"];
