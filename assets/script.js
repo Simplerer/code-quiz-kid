301ef521a37342753dd18a41380e96fcc9c5327b
A Stone <simplererstone@gmail.com>
2022-12-05 16:08:09 -0500

    set timerstop as a varibable for scoring, set to localstorage

--- assets/script.js
+++ assets/script.js
@@ -8,6 +8,7 @@ var stopTimer = document.getElementById("stopTimer");
 var cursor = 0;
 var secondsLeft = 60;
 var timerInterval;
+var scoredTime;
 
 
 opener.addEventListener("click", function() {
@@ -15,47 +16,44 @@ opener.addEventListener("click", function() {
     showQuestions();
 })
 
-var Scoring = function() {
-    
-}
-
-
 var countDown = function() {
     var tickTock = " seconds";
-        if (secondsLeft === 1) {
+
+    
+    if (secondsLeft === 1) {
             tickTock = " second";
         }
         timer.textContent = secondsLeft + tickTock;
-
-}
-
-var startTimer = function() {
-    countDown();
         
-    timerInterval = setInterval(function() {
-        secondsLeft--;
+    }
+    
+    var startTimer = function() {
         countDown();
-
-        if (secondsLeft === 0) {
-            clearInterval(timerInterval);
-            timer.textContent = "Out of Time!";
-            //run form page for highscores
+        
+        timerInterval = setInterval(function() {
+            secondsLeft--;
+            countDown();
             
-        }
-
-    }, 1000)
-
-}
-var showQuestions = function() {
+            if (secondsLeft === 0) {
+                clearInterval(timerInterval);
+                timer.textContent = "Out of Time!";
+                //run form page for highscores
+                
+            }
+            
+        }, 1000)
+        
+    }
+    var showQuestions = function() {
     for (var item of containers) {
         
         if (item.dataset.index != cursor) {
-
+            
             item.style.display = "none";
         } else {
             item.style.display = "block";
         }
-}
+    }
 }
 
 var nextQuestion = function(event) {
@@ -65,21 +63,29 @@ var nextQuestion = function(event) {
     if (element.matches(".container button"))  {
         var dataAnswer = element.getAttribute("data-name");
         if (dataAnswer === "correct") {
-
-        if (cursor < containers.length - 1) {
-            cursor++;
-        } 
+            
+            if (cursor < containers.length - 1) {
+                cursor++;
+            } 
         } else if (dataAnswer === "done") {
+            scoredTime = secondsLeft;
+            console.log(scoredTime);
             Scoring();
-        }    else {
-        secondsLeft = secondsLeft - 5;
+        }else {
+            secondsLeft = secondsLeft - 5;
         }
-
+        
         
         showQuestions();
     }
 }
 
+var Scoring = function() {
+    clearInterval(timerInterval)
+    console.log(scoredTime);
+    localStorage.setItem("timeScore", scoredTime);
+
+}
 
 
 
